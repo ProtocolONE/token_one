98a3655d39df7c132d4088f0976d4203c0c31234
nikolay.bondarenko <misterionkell@gmail.com>
2018-07-09 19:18:18 +0300

    Add openzeppelin-solidity deps and fix sol files

--- contracts/PoneCoin.sol
+++ contracts/PoneCoin.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.19;
 
-import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 contract PoneCoin is MintableToken {
     string public name = "PONE COIN";
