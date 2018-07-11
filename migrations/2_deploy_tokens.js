dc2d55547f258560675508f1973456fe4b734a4f
kirill.vostretsov <kirill.vostretsov@syncopate.ru>
2018-07-11 15:29:27 +0300

    Name fix

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -1,5 +1,5 @@
-const GamenetCoin = artifacts.require('./OneSmartToken.sol');
-const GamenetCoinCrowdsale = artifacts.require('./OneCrowdsale.sol');
+const OneCoin = artifacts.require('./OneSmartToken.sol');
+const OneCrowdsale = artifacts.require('./OneCrowdsale.sol');
 
 module.exports = function (deployer, network, accounts) {
   const openingTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 10; // 10 seconds from now;
@@ -7,5 +7,5 @@ module.exports = function (deployer, network, accounts) {
   const rate = new web3.BigNumber(1000);
   const wallet = accounts[0]; // Second account
 
-  return deployer.then(() => deployer.deploy(OneSmartToken)).then(() => deployer.deploy(OneCrowdsale, openingTime, closingTime, rate, wallet, GamenetCoin.address));
+  return deployer.then(() => deployer.deploy(OneCoin)).then(() => deployer.deploy(OneCrowdsale, openingTime, closingTime, rate, wallet, OneCoin.address));
 };
