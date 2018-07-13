a64fd16b5ca0e2bcbcdcb1226bc331583949ed91
nikolay.bondarenko <misterionkell@gmail.com>
2018-07-13 18:05:11 +0300

    Cleanup Administrable

--- contracts/ownership/Administrable.sol
+++ contracts/ownership/Administrable.sol
@@ -7,54 +7,22 @@ contract Administrable is Ownable {
   using SafeMath for uint256;
 
   address[] public adminsForIndex;
-  address[] public superAdminsForIndex;
   mapping (address => bool) public admins;
-  mapping (address => bool) public superAdmins;
   mapping (address => bool) private processedAdmin;
-  mapping (address => bool) private processedSuperAdmin;
 
   event AddAdmin(address indexed admin);
   event RemoveAdmin(address indexed admin);
-  event AddSuperAdmin(address indexed admin);
-  event RemoveSuperAdmin(address indexed admin);
 
   modifier onlyAdmins {
-    if (msg.sender != owner && !superAdmins[msg.sender] && !admins[msg.sender]) revert();
+    if (msg.sender != owner && !admins[msg.sender]) revert();
     _;
-  }
-
-  modifier onlySuperAdmins {
-    if (msg.sender != owner && !superAdmins[msg.sender]) revert();
-    _;
-  }
-
-  function totalSuperAdminsMapping() public view returns (uint256) {
-    return superAdminsForIndex.length;
-  }
-
-  function addSuperAdmin(address admin) public onlySuperAdmins {
-    require(admin != address(0));
-    superAdmins[admin] = true;
-    if (!processedSuperAdmin[admin]) {
-      superAdminsForIndex.push(admin);
-      processedSuperAdmin[admin] = true;
-    }
-
-    emit AddSuperAdmin(admin);
-  }
-
-  function removeSuperAdmin(address admin) public onlySuperAdmins {
-    require(admin != address(0));
-    superAdmins[admin] = false;
-
-    emit RemoveSuperAdmin(admin);
   }
 
   function totalAdminsMapping() public view returns (uint256) {
     return adminsForIndex.length;
   }
 
-  function addAdmin(address admin) public onlySuperAdmins {
+  function addAdmin(address admin) public onlyOwner {
     require(admin != address(0));
     admins[admin] = true;
     if (!processedAdmin[admin]) {
@@ -65,7 +33,7 @@ contract Administrable is Ownable {
     emit AddAdmin(admin);
   }
 
-  function removeAdmin(address admin) public onlySuperAdmins {
+  function removeAdmin(address admin) public onlyOwner {
     require(admin != address(0));
     admins[admin] = false;
 
