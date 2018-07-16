91552e03c54f75e589d550013fb54b10fb147fbc
nikolay.bondarenko <misterionkell@gmail.com>
2018-07-16 16:46:12 +0300

    Add travis build script

--- scripts/test.sh
+++ scripts/test.sh
@@ -56,10 +56,7 @@ fi
 
 if [ "$SOLIDITY_COVERAGE" = true ]; then
   node_modules/.bin/solidity-coverage
-
-  if [ "$CONTINUOUS_INTEGRATION" = true ]; then
-    cat coverage/lcov.info | node_modules/.bin/coveralls
-  fi
+  cat coverage/lcov.info | node_modules/.bin/coveralls
 else
   node_modules/.bin/truffle test "$@"
 fi
