475fcd9ef2109ce4fda5a407e26c4d90bd2a5266
nikolay.bondarenko <misterionkell@gmail.com>
2018-07-16 17:51:10 +0300

    Fix sed path to solcover

--- scripts/test.sh
+++ scripts/test.sh
@@ -38,7 +38,7 @@ start_ganache() {
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209,1000000000000000000000000"
   )
 
-  sed -i -e 's/norpc: false/norpc: true/g' ../.solcover.js
+  sed -i -e 's/norpc: false/norpc: true/g' .solcover.js
 
   if [ "$SOLIDITY_COVERAGE" = true ]; then
     node_modules/.bin/testrpc-sc --gasLimit 0xfffffffffff --port "$ganache_port" "${accounts[@]}" > /dev/null &
