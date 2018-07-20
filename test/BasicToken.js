116ab210aaad979462d58afbf322bf3dec8c6e17
kirill.vostretsov <kirill.vostretsov@syncopate.ru>
2018-07-20 13:16:17 +0300

    Linter Fix

--- test/BasicToken.js
+++ test/BasicToken.js
@@ -11,20 +11,20 @@ contract('BasicToken', (accounts) => {
   });
 
   it('should return correct balances after transfer', async () => {
-    const token = await BasicTokenMock.new(accounts[0], 100);
-    const transfer = await token.transfer(accounts[1], 100);
+    let token = await BasicTokenMock.new(accounts[0], 100);
+    await token.transfer(accounts[1], 100);
 
-    const firstAccountBalance = await token.balanceOf(accounts[0]);
+    let firstAccountBalance = await token.balanceOf(accounts[0]);
     assert.equal(firstAccountBalance, 0);
 
-    const secondAccountBalance = await token.balanceOf(accounts[1]);
+    let secondAccountBalance = await token.balanceOf(accounts[1]);
     assert.equal(secondAccountBalance, 100);
   });
 
   it('should throw an error when trying to transfer more than balance', async () => {
     const token = await BasicTokenMock.new(accounts[0], 100);
     try {
-      const transfer = await token.transfer(accounts[1], 101);
+      await token.transfer(accounts[1], 101);
       assert.fail('should have thrown before');
     } catch (error) {
       assertJump(error);
@@ -34,7 +34,7 @@ contract('BasicToken', (accounts) => {
   it('should throw an error when trying to transfer to 0x0', async () => {
     const token = await BasicTokenMock.new(accounts[0], 100);
     try {
-      const transfer = await token.transfer(0x0, 100);
+      await token.transfer(0x0, 100);
       assert.fail('should have thrown before');
     } catch (error) {
       assertJump(error);
