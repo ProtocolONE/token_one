8a64cd0fe4d1a6b4741b59372df3e8553bc0a1e3
kirill.vostretsov <kirill.vostretsov@syncopate.ru>
2018-07-23 12:33:52 +0300

    Test fixes

--- test/BasicToken.js
+++ test/BasicToken.js
@@ -5,7 +5,7 @@ const BasicTokenMock = artifacts.require('./helpers/BasicTokenMock.sol');
 contract('BasicToken', (accounts) => {
   it('should return the correct totalSupply after construction', async () => {
     const token = await BasicTokenMock.new(accounts[0], 100);
-    const totalSupply = await token.totalSupply();
+    const totalSupply = await token.totalSupply.call(accounts[0]);
 
     assert.equal(totalSupply, 100);
   });
