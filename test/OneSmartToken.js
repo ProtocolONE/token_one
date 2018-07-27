53d866ed797ba4e6dba0dff53cc04850e687e6cf
Dmitriy Sinichkin <dmitriy.sinichkin@gmail.com>
2018-07-27 16:04:03 +0300

    OneSmartToken test.

--- test/OneSmartToken.js
+++ test/OneSmartToken.js
@@ -18,7 +18,7 @@ contract('OneSmartToken', (accounts) => {
   it('should start with a totalSupply of 0', async () => {
     const totalSupply = await token.totalSupply();
 
-    assert.equal(totalSupply, 0);
+    assert.equal(totalSupply.toNumber(), 0);
   });
 
   it('should return mintingFinished false after construction', async () => {
