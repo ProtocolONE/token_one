3b832791de79e2757250f8c8b5fab4b4a3439dde
Dmitriy Sinichkin <dmitriy.sinichkin@gmail.com>
2018-07-31 07:58:48 +0300

    OneCrowdSale test

--- test/helpers/OneCrowdsaleMock.sol
+++ test/helpers/OneCrowdsaleMock.sol
@@ -6,6 +6,13 @@ import '../../contracts/OneCrowdsale.sol';
 // mock class using PreSaleCrowdsale
 contract OneCrowdsaleMock is OneCrowdsale {
 
+  event SendDealDepositData(
+    address refundWallet,
+    uint256 depositedETH,
+    uint256 depositedTokens,
+    uint256 transferred
+  );
+
   function OneCrowdsaleMock(address _wallet, address _walletTeam, address _walletAdvisers, 
     address _walletOperating, address _walletReserve, address _walletBounty, uint256 _openingTime, uint256 _closingTime,
     uint256 _softCap, uint256 _hardCap) 
@@ -33,5 +40,25 @@ contract OneCrowdsaleMock is OneCrowdsale {
   function getBonusRateMocked(address _benefic) public view returns (uint256) {
     uint256 bRate = getBonusRate(_benefic);
     return bRate;
+  }
+
+  function getWalletRefundData(address _wallet) {
+      DealDeposit storage deposit = depositMap[_wallet];
+
+      emit SendDealDepositData(
+          deposit.refundWallet,
+          deposit.depositedETH,
+          deposit.depositedTokens,
+          deposit.transferred
+      );
+  }
+
+  function setDepositTokens(address _wallet, uint256 _tokens) {
+      if (depositMap[_wallet].refundWallet == address(0)) {
+          return;
+      }
+
+      DealDeposit storage deposit = depositMap[_wallet];
+      deposit.depositedTokens = _tokens;
   }
 }
